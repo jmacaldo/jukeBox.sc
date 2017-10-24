4511f9b33d5814a499a6a1f2dfe5acb41ef21111
Joven Macaldo <jmacaldo@me.com>
2017-10-24 14:39:03 -0400

    initial push to dev branch

---
+++ js/sc.js
@@ -0,0 +1,54 @@
+$( document ).ready(function() {
+
+SC.initialize({
+ client_id: 'fd4e76fc67798bfa742089ed619084a6'
+});
+// ALL CODE GOES BELOW THIS LINE
+$("#submit").click(function(){
+    $(".well").slideDown("fast", function() {
+     var query = $('#search').val();
+        console.log(query);
+    $("#stop").slideDown("fast");
+     SC.get('/tracks', {
+     q: query,}).then(function(tracks) {
+//display fallback artwork if none is present in SC
+     if (tracks[0].artwork_url == null) {
+       tracks[0].artwork_url = "imgs/doge.jpg"
+     } ;
+     console.log(tracks);
+     console.log(tracks[0].user.username); //username
+     console.log(tracks[0].permalink_url);
+     console.log(tracks[0].genre);
+//display artist info and artwork. first song in the array is the one displayed/played
+     $("#art").html("<img src='"+tracks[0].artwork_url+"'/>");
+     $("#htmlTitle").html("<p>"+tracks[0].title+"</p></br>");
+     $("#username").html("<p>Username: "+tracks[0].user.username+"</p></br>");
+     $("#genre").html("<p>Album: "+tracks[0].genre+"</p></br>");
+     $("#url").html("<a href="+tracks[0].permalink_url+">Link to SoundCloud</a>");
+//begin streaming when artwork is clicked
+     $("#art").click(function(){
+       SC.stream("/tracks/"+tracks[0].id).then(function(player){
+         player.play();
+//stop button functionality
+
+
+       $("#stop").click(function(){
+         player.pause();
+       });
+          });
+
+
+
+
+        }); //sc.stream
+      }); //click function
+
+
+
+
+   }); //search query callback closing tag
+
+ }); //submit function closing tag
+
+
+}); //document ready closing tag!

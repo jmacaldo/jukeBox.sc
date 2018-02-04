a512d817822321db009f84663b608ce01c06514a
Joven Macaldo <jmacaldo@me.com>
2018-02-04 10:24:32 -0500

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jukeBox.sc
+
+User must provide own SoundCloud API key and insert it on line 6 of file
+js/oopsc.js
--- js/oopsc.js
+++ js/oopsc.js
@@ -3,28 +3,31 @@ $( document ).ready(function() {
 var track =[]
 class Jukebox {
   constructor(){
-    SC.initialize({client_id: 'SUPPLY SC API KEY HERE'});
+    SC.initialize({client_id: 'SUPPLY API KEY HERE'});
     console.log('init fired!');
   }
 
   search(e){
+
+
     var query = $('#search').val();
     SC.get('/tracks', {
       q: query,
     }).then(function(res) {
+      console.log('log fire');
       track.push(res[0])
-      $(".well").slideDown("fast", function() {
+      $(".well").slideDown("fast");
         $("#stop").slideDown("fast");
         $("#play").slideDown("fast");
-        if (track[0].artwork_url == null) {
-          track[0].artwork_url = "imgs/doge.jpg"
+        if (res[0].artwork_url == null) {
+          res[0].artwork_url = "imgs/doge.jpg"
         } ;
-        $("#art").html("<img src='"+track[0].artwork_url+"'/>");
-        $("#htmlTitle").html("<p>"+track[0].title+"</p></br>");
-        $("#username").html("<p>Username: "+track[0].user.username+"</p></br>");
-        $("#genre").html("<p>Album: "+track[0].genre+"</p></br>");
-        $("#url").html("<a href="+track[0].permalink_url+">Link to SoundCloud</a>");
-      })
+        $("#art").html("<img src='"+res[0].artwork_url+"'/>");
+        $("#htmlTitle").html("<p>"+res[0].title+"</p></br>");
+        $("#username").html("<p>Username: "+res[0].user.username+"</p></br>");
+        $("#genre").html("<p>Album: "+res[0].genre+"</p></br>");
+        $("#url").html("<a href="+res[0].permalink_url+">Link to SoundCloud</a>");
+
     })
   }
 
